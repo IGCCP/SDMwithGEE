81945d44c116bdefb091538472c11fbffbaa6a46
wli <39733492+IGCCP@users.noreply.github.com>
2024-01-23 14:31:23 -0800

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,1 +1,22 @@
+## https://sites.google.com/utk.edu/geog-414/resources/software
+#Software
 
+Miniconda or Anaconda
+
+Visual Studio Code
+
+Git
+
+Google Earth Engine
+
+Google Colab
+
+GitHub Education
+
+# Package repositories
+
+PyPI
+
+Anaconda
+
+Conda-forge
